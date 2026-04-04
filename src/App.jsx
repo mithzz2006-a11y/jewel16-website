443b112bf8113d6e56e170065447aa2bf3df9195
mithzz2006-a11y <mithzz2006@gmail.com>
2026-04-04 14:38:44 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ background: "black", color: "white", height: "100vh", textAlign: "center", paddingTop: "100px" }}>
+      <h1 style={{ color: "maroon", fontSize: "50px" }}>Jewel16 💎</h1>
+      <p>Luxury Jewellery Website</p>
+    </div>
+  );
+}
