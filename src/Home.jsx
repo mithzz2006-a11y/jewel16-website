6db6fbb21f7b7b663cbe2d7a39aa3f2f13ee0a97
mithzz2006-a11y <mithzz2006@gmail.com>
2026-04-08 08:47:51 +0530

    fix home

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,10 +4,7 @@ export default function Home({ setPage }) {
       <h1 style={{ color: "maroon" }}>JEWEL16 💎</h1>
       <p>Luxury Redefined</p>
 
-      <button 
-        onClick={() => setPage("products")}
-        style={{ marginTop: "20px", padding: "10px 20px", background: "maroon", color: "white", border: "none" }}
-      >
+      <button onClick={() => setPage("products")}>
         Shop Now
       </button>
     </div>
