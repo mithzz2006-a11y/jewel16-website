e3d50a724b8a82eef66c4beb10422e6f6992af79
mithzz2006-a11y <mithzz2006@gmail.com>
2026-04-08 08:28:33 +0530

    home changed error

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,15 @@
-<button 
-  onClick={() => window.location.href="/products"}
-  style={{ marginTop: "20px", padding: "10px 20px", background: "maroon", color: "white", border: "none" }}
->
-  Shop Now
-</button>
+export default function Home({ setPage }) {
+  return (
+    <div style={{ background: "#0a0a0a", color: "white", textAlign: "center", padding: "100px" }}>
+      <h1 style={{ color: "maroon" }}>JEWEL16 💎</h1>
+      <p>Luxury Redefined</p>
+
+      <button 
+        onClick={() => setPage("products")}
+        style={{ marginTop: "20px", padding: "10px 20px", background: "maroon", color: "white", border: "none" }}
+      >
+        Shop Now
+      </button>
+    </div>
+  );
+}
