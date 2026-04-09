16c3926b9dd25f14b29a929129a01df613ab8ab3
mithzz2006-a11y <mithzz2006@gmail.com>
2026-04-09 10:19:38 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+
+export default function App() {
+  const [page, setPage] = useState("home");
+  const [cart, setCart] = useState([]);
+
+  return (
+    <div>
+      {page === "home" && <Home setPage={setPage} />}
+      
+      {page === "products" && (
+        <Products 
+          cart={cart} 
+          setCart={setCart} 
+          setPage={setPage} 
+        />
+      )}
+      
+      {page === "cart" && (
+        <Cart 
+          cart={cart} 
+          setCart={setCart} 
+          setPage={setPage} 
+        />
+      )}
+    </div>
+  );
+}
