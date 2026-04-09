c45c850639cbf1768058ecf44d64193fef57d11e
mithzz2006-a11y <mithzz2006@gmail.com>
2026-04-09 10:29:09 +0530

    fixed home syntax

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
 export default function Home({ setPage }) {
   return (
-    <div style={{ background: "#0a0a0a", color: "white", textAlign: "center", padding: "100px" }}>
+    <div style={{
+      background: "#0a0a0a",
+      color: "white",
+      textAlign: "center",
+      padding: "100px"
+    }}>
       <h1 style={{ color: "maroon" }}>JEWEL16 💎</h1>
       <p>Luxury Redefined</p>
 
-      <button onClick={() => setPage("products")}>
+      <button
+        onClick={() => setPage("products")}
+        style={{
+          marginTop: "20px",
+          padding: "10px 20px",
+          background: "maroon",
+          color: "white",
+          border: "none",
+          cursor: "pointer"
+        }}
+      >
         Shop Now
       </button>
     </div>
