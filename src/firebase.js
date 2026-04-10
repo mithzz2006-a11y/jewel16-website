3cbd0ee409b217847ae54daf7fa1b9c80d65344e
mithzz2006-a11y <mithzz2006@gmail.com>
2026-04-10 14:16:49 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,10 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_REAL_API_KEY",
+  apiKey: "AIzaSyCNZA83__KERpYUhMoBz7YIxXoAUezDPus",
   authDomain: "jewel16-95f3f.firebaseapp.com",
   projectId: "jewel16-95f3f",
-  storageBucket: "jewel16-95f3f.appspot.com",
-  messagingSenderId: "PASTE_ID",
-  appId: "PASTE_APP_ID"
+  storageBucket: "jewel16-95f3f.firebasestorage.app",
+  messagingSenderId: "613112343850",
+  appId: "1:613112343850:web:35b0ec24389b0c32b7c253",
+  measurementId: "G-YTME7S1Y0G"
 };
-
-const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
