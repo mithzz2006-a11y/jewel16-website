52cced76de22f6bf5125d4a706db1999cd7e9e50
mithzz2006-a11y <mithzz2006@gmail.com>
2026-04-13 14:44:13 +0530

    Create Checkout.jsx

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,12 @@
+export default function Checkout({ cart }) {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Checkout</h1>
+
+      <p>Secure Payment Gateway 🔒</p>
+      <p>Fast Delivery 🚚</p>
+
+      <button>Pay Now</button>
+    </div>
+  );
+}
