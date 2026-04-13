39573e8af25585a1897555de4b41b36e5c512057
mithzz2006-a11y <mithzz2006@gmail.com>
2026-04-13 08:42:48 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,18 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCNZA83__KERpYUhMoBz7YIxXoAUezDPus",
   authDomain: "jewel16-95f3f.firebaseapp.com",
   projectId: "jewel16-95f3f",
-  storageBucket: "jewel16-95f3f.appspot.com",
+  storageBucket: "jewel16-95f3f.firebasestorage.app",
   messagingSenderId: "613112343850",
-  appId: "1:613112343850:web:35b0ec24389b0c32b7c253"
+  appId: "1:613112343850:web:35b0ec24389b0c32b7c253",
+  measurementId: "G-YTME7S1Y0G"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export Firestore and Auth
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
