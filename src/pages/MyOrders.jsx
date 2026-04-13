8a51a366fef3a0d631ec998f955788c01a838b11
mithzz2006-a11y <mithzz2006@gmail.com>
2026-04-13 13:44:26 +0530

    Update MyOrders.jsx

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -1,23 +1,23 @@
 import { useEffect, useState } from "react";
-import { db, auth } from "../firebase";
+import { db } from "../firebase";
 import { collection, getDocs } from "firebase/firestore";
 
-export default function MyOrders() {
+export default function MyOrders({ user }) {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    async function load() {
-      const snap = await getDocs(collection(db, "orders"));
-      const user = auth.currentUser;
-
-      const data = snap.docs
-        .map(d => d.data())
-        .filter(o => o.email === user.email);
-
-      setOrders(data);
-    }
     load();
   }, []);
+
+  const load = async () => {
+    const snap = await getDocs(collection(db, "orders"));
+
+    const data = snap.docs
+      .map(d => d.data())
+      .filter(o => o.email === user.email);
+
+    setOrders(data);
+  };
 
   return (
     <div style={{ padding: "20px" }}>
@@ -25,8 +25,8 @@ export default function MyOrders() {
 
       {orders.map((o, i) => (
         <div key={i}>
-          <p>Total: ₹{o.total}</p>
-          <p>Status: {o.status}</p>
+          <p>₹{o.total}</p>
+          <p>{o.status}</p>
         </div>
       ))}
     </div>
