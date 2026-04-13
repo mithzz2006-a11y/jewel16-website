61bfa56f2c456ee34dd785e32541d2006d3327ec
mithzz2006-a11y <mithzz2006@gmail.com>
2026-04-13 11:52:28 +0530

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,17 +1,9 @@
 export default function Cart({ cart, setPage }) {
-
   const total = cart.reduce((sum, item) => sum + item.price, 0);
 
   return (
-    <div style={{
-      background: "black",
-      color: "white",
-      minHeight: "100vh",
-      padding: "20px"
-    }}>
+    <div style={{ padding: "20px" }}>
       <h1>Your Cart</h1>
-
-      {cart.length === 0 && <p>No items</p>}
 
       {cart.map((item, i) => (
         <div key={i}>
