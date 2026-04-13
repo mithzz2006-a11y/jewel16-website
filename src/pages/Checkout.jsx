bd8997989202b4552fb2a019bb5cee836e845fbb
mithzz2006-a11y <mithzz2006@gmail.com>
2026-04-13 11:53:01 +0530

    Update Checkout.jsx

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,46 +1,19 @@
-import { db, auth } from "../firebase";
-import { collection, addDoc } from "firebase/firestore";
-
-export default function Checkout({ cart, setPage }) {
-
+export default function Checkout({ cart }) {
   const total = cart.reduce((sum, item) => sum + item.price, 0);
 
-  const placeOrder = async () => {
-    const user = auth.currentUser;
-
-    if (!user) {
-      alert("Login required");
-      return;
-    }
-
-    await addDoc(collection(db, "orders"), {
-      email: user.email,
-      items: cart,
-      total,
-      status: "Placed",
-      createdAt: new Date()
-    });
-
-    alert("Order placed ✅");
-
-    setPage("orders");
-  };
-
   return (
-    <div style={{ background: "black", color: "white", padding: "20px" }}>
+    <div style={{ padding: "20px" }}>
       <h1>Checkout</h1>
 
       {cart.map((item, i) => (
         <div key={i}>
-          <p>{item.name}</p>
+          <p>{item.name} - ₹{item.price}</p>
         </div>
       ))}
 
       <h2>Total: ₹{total}</h2>
 
-      <button onClick={placeOrder}>
-        Place Order
-      </button>
+      <button>Pay Now</button>
     </div>
   );
 }
