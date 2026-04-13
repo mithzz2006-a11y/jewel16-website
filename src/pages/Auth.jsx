cd4e9f70e057025fe90ad73cc33eb15764c4f2c6
mithzz2006-a11y <mithzz2006@gmail.com>
2026-04-13 15:22:20 +0530

    Update Auth.jsx

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,1 +1,38 @@
+import { useState } from "react";
+import { auth } from "../firebase";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 
+export default function Auth({ setUser }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    const res = await signInWithEmailAndPassword(auth, email, password);
+    setUser(res.user);
+  };
+
+  const signup = async () => {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    setUser(res.user);
+  };
+
+  return (
+    <div style={box}>
+      <h1>JEWEL16 💎</h1>
+
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+
+      <button onClick={login}>Login</button>
+      <button onClick={signup}>Signup</button>
+    </div>
+  );
+}
+
+const box = {
+  textAlign: "center",
+  marginTop: "100px",
+};
