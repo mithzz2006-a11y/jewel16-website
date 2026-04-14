4dae7f1fffe3b95b4279eb41f496e0da636f474f
mithzz2006-a11y <mithzz2006@gmail.com>
2026-04-14 14:12:19 +0530

    Update ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,1 +1,45 @@
+export default function ProductCard({ product, setCart }) {
+  return (
+    <div style={card}>
+      <img src={product.image} style={img} />
 
+      <h3>{product.name}</h3>
+      <p style={{ color: "maroon", fontWeight: "bold" }}>
+        ₹{product.price}
+      </p>
+
+      <button
+        style={btn}
+        onClick={() => setCart((prev) => [...prev, product])}
+      >
+        Add to Cart
+      </button>
+    </div>
+  );
+}
+
+const card = {
+  border: "1px solid #eee",
+  padding: "15px",
+  borderRadius: "10px",
+  textAlign: "center",
+  transition: "0.3s",
+  boxShadow: "0 2px 10px rgba(0,0,0,0.05)",
+};
+
+const img = {
+  width: "100%",
+  height: "180px",
+  objectFit: "cover",
+  borderRadius: "10px",
+};
+
+const btn = {
+  marginTop: "10px",
+  padding: "10px",
+  width: "100%",
+  background: "black",
+  color: "white",
+  border: "none",
+  borderRadius: "5px",
+};
