5ed191af525a333a699949476e31efa422c24050
mithzz2006-a11y <mithzz2006@gmail.com>
2026-04-14 14:18:29 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,10 +6,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyCNZA83__KERpYUhMoBz7YIxXoAUezDPus",
   authDomain: "jewel16-95f3f.firebaseapp.com",
   projectId: "jewel16-95f3f",
-  storageBucket: "jewel16-95f3f.firebasestorage.app",
+  storageBucket: "jewel16-95f3f.appspot.com",
   messagingSenderId: "613112343850",
   appId: "1:613112343850:web:35b0ec24389b0c32b7c253",
-  measurementId: "G-YTME7S1Y0G"
 };
 
 const app = initializeApp(firebaseConfig);
