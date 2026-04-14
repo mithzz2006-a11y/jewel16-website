5b7019fbfc93e63f2f8ab0c34252cce76421d1b1
mithzz2006-a11y <mithzz2006@gmail.com>
2026-04-14 14:19:32 +0530

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,27 +1,24 @@
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
 import ProductCard from "../components/ProductCard";
 
-const products = [
-  {
-    id: 1,
-    name: "Gold Ring",
-    price: 500,
-    image: "https://images.unsplash.com/photo-1617038260897-41a1f14a8ca0"
-  },
-  {
-    id: 2,
-    name: "Diamond Necklace",
-    price: 2000,
-    image: "https://images.unsplash.com/photo-1599643478518-a784e5dc4c8f"
-  },
-  {
-    id: 3,
-    name: "Gold Chain",
-    price: 1500,
-    image: "https://images.unsplash.com/photo-1602751584552-8ba73aad10e1"
-  },
-];
-
 export default function Products({ setCart }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const snap = await getDocs(collection(db, "products"));
+      const list = snap.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setProducts(list);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div style={grid}>
       {products.map((p) => (
