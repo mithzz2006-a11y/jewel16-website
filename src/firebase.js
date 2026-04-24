b10bdff57003b4e280087c3ec99459d83db91cc9
mithzz2006-a11y <mithzz2006@gmail.com>
2026-04-24 12:14:07 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+/* 🔐 CONFIG */
 const firebaseConfig = {
   apiKey: "AIzaSyCNZA83__KERpYUhMoBz7YIxXoAUezDPus",
   authDomain: "jewel16-95f3f.firebaseapp.com",
   projectId: "jewel16-95f3f",
-  storageBucket: "jewel16-95f3f.firebasestorage.app",
+  storageBucket: "jewel16-95f3f.appspot.com", // 🔥 FIXED
   messagingSenderId: "613112343850",
   appId: "1:613112343850:web:35b0ec24389b0c32b7c253",
-  measurementId: "G-YTME7S1Y0G"
 };
 
+/* 🚀 INIT */
 const app = initializeApp(firebaseConfig);
 
+/* 🔥 EXPORTS */
 export const auth = getAuth(app);
 export const db = getFirestore(app);
